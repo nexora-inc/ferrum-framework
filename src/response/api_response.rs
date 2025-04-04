c43bcb0a91b0814d62f6ac270fe5df6a6ea3f2c6
Shan Padayhag <shanpadayhag@gmail.com>
2025-04-04 08:33:20 +0800

    FF-22 Removed allowed headers

--- src/response/api_response.rs
+++ src/response/api_response.rs
@@ -35,8 +35,6 @@ impl ApiResponse {
     Response::builder()
       .status(status_code)
       .header("Content-Type", "application/json")
-      .header("Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token")
-      .header("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PUT,PATCH,DELETE")
       .body(serde_json::to_string(&data).unwrap_or_default())
         .unwrap()
   }
